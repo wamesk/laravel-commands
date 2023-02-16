cd64f63a3c706a2800059d6eabbca27137819418
Stanislav Červeňák <djsucho@gmail.com>
2023-02-16 15:38:59 +0100

    EDIT config

--- config/wame-commands.php
+++ config/wame-commands.php
@@ -4,29 +4,29 @@ declare(strict_types = 1);
 
 return [
     // Version of ApiController you want to develop (v1, v2, v3, null)
-    //    'version' => 'v1', // Default: null
+    //'version' => 'v1', // Default: null
 
     // Type of id your project is using (options: id (basic integer), uuid, ulid)
-    //    'id-type' => 'uuid', // Default: ulid
+    //'id-type' => 'uuid', // Default: ulid
 
     // Enable or disable sorting in migration and model
-    //    'sorting' => false, // Default: true
+    //'sorting' => false, // Default: true
 
     // Per page pagination default
-    //    'per_page' => 10, // Default: 10
+    //'per_page' => 10, // Default: 10
 
     // What language mutations should it generate. All will contain EN phrases.
-    //    'langs' => ['en'], // Default: en
+    //'langs' => ['en'], // Default: en
 
     // You can disable commands that wame:make will run. By default, all will run.
     'make' => [
-        //        'api-controllers' => false,
-        //        'events' => false,
-        //        'lang' => false,
-        //        'listeners' => false,
-        //        'migration' => false,
-        //        'model' => false,
-        //        'nova' => false,
-        //        'observer' => false,
+        //'api-controllers' => false,
+        //'events' => false,
+        //'lang' => false,
+        //'listeners' => false,
+        //'migration' => false,
+        //'model' => false,
+        //'nova' => false,
+        //'observer' => false,
     ],
 ];
